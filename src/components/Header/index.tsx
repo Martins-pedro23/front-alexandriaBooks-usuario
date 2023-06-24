aa523319225f37ca34c91c923e47b810867b19c5
Kyoudan <fogoste@gmail.com>
2023-06-24 17:34:02 -0300

    feat: adding theme dark in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import Icon from "../../../public/Icon.png";
 
 export const Header = () => {
   return (
-    <div className="w-screen bg-white flex flex-row justify-between items-center p-4">
+    <div className="w-screen bg-white flex flex-row justify-between items-center p-4 dark:bg-zinc-900">
       <img src="./Icon.png" alt="Logo" className="w-20 h-20" />
       <div className="flex flex-row items-center">
         <input
           placeholder="Buscar livro"
-          className="bg-zinc-100 text-xl pt-2 pb-2 pl-4 pr-4 rounded-l outline-none focus:bg-zinc-200 transition-all"
+          className="bg-zinc-100 text-xl pt-2 pb-2 pl-4 pr-4 rounded-l outline-none focus:bg-zinc-200 transition-all dark:bg-zinc-800 dark:text-white dark:focus:bg-zinc-700"
         />
         <button className="bg-primary w-11 h-11 rounded-r flex items-center justify-center text-white hover:bg-primary-light-100 transition-all">
           <Search />
