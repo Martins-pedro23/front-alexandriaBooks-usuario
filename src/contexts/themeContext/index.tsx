3d834d0f445765983170a963c81b7b3a3853be18
Kyoudan <fogoste@gmail.com>
2023-06-24 21:30:53 -0300

    fix: fixing localstorage rendering in SSR

--- src/contexts/themeContext/index.tsx
+++ src/contexts/themeContext/index.tsx
@@ -1,51 +1,53 @@
 "use client";
 
 import {
-  useContext,
-  createContext,
-  useState,
-  useEffect,
-  Dispatch,
-  SetStateAction,
+    useContext,
+    createContext,
+    useState,
+    useEffect,
+    Dispatch,
+    SetStateAction,
 } from "react";
 
 interface IProps {
-  children: JSX.Element;
+    children: JSX.Element;
 }
 
 interface IContext {
-  theme: "dark" | "light";
-  toggleTheme: () => void;
+    theme: "dark" | "light";
+    toggleTheme: () => void;
 }
+
+const isBrowser = typeof window !== "undefined";
 
 const ThemeContext = createContext({} as IContext);
 
 export const ThemeContextProvider = ({ children }: IProps) => {
-  const [theme, setTheme] = useState<"dark" | "light">(
-    localStorage.theme || "light"
-  );
+    const [theme, setTheme] = useState<"dark" | "light">(
+        (isBrowser && (localStorage.getItem("theme") as "dark")) || "light"
+    );
 
-  useEffect(() => {
-    const root = window.document.documentElement;
+    useEffect(() => {
+        const root = window.document.documentElement;
 
-    const remove = theme === "dark" ? "light" : "dark";
+        const remove = theme === "dark" ? "light" : "dark";
 
-    root.classList.remove(remove);
-    root.classList.add(theme);
-    localStorage.setItem("theme", theme);
-  }, [theme]);
+        root.classList.remove(remove);
+        root.classList.add(theme);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
 
-  const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
+    const toggleTheme = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
 };
 
 export const useTheme = () => {
-  return useContext(ThemeContext);
+    return useContext(ThemeContext);
 };
