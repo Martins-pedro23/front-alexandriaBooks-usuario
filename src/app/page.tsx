a41a0ca2e29a2dcdc93760e3f95e50988b1e24c0
Kyoudan <fogoste@gmail.com>
2023-06-24 21:55:40 -0300

    style: adding prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import { ThemeContextProvider } from "@/contexts/themeContext";
 
 export default function Home() {
   return (
-
-      <div>
-        <div className="bg-primary text-white w-screen text-center font-montserrat font-bold">
-          Bem vindo à Alexandria Books
-        </div>
-        <h1>oi</h1>
+    <div>
+      <div className="bg-primary text-white w-screen text-center font-montserrat font-bold">
+        Bem vindo à Alexandria Books
       </div>
-
+      <h1>oi</h1>
+    </div>
   );
 }
